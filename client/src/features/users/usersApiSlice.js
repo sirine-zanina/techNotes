b5a8990ab195782ecf1d4a94ccd2dffda569d15f
reverie-kyrene <107148650+reverie-syr@users.noreply.github.com>
2023-10-22 18:45:32 +0200

    Redux & RTK Query

--- client/src/features/users/usersApiSlice.js
+++ client/src/features/users/usersApiSlice.js
@@ -1,10 +1,55 @@
-import {
-    createSelector,
-    createEntityAdapter
-} from "@reduxjs/toolkit";
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
 import { apiSlice } from "../../app/api/apiSlice";
 
-const usersAdapter = createEntityAdapter({})
+const usersAdapter = createEntityAdapter({});
 
-const intialState = usersAdapter.getIntialState();
+const initialState = usersAdapter.getInitialState();
 
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      validateStatus: (response, result) => {
+        return response.status === 200 && !result.isError;
+      },
+      keepUnusedDataFor: 5, // change it to 60 seconds when deployed to production
+      transformResponse: (responseData) => {
+        const loadedUsers = responseData.map((user) => {
+          user.id = user._id;
+          return user;
+        });
+
+        return usersAdapter.setAll(initialState, loadedUsers);
+      },
+      providesTags: (result, error, arg) => {
+        if (result?.ids) {
+          return [
+            { type: "User", id: "LIST" },
+            ...result.ids.map((id) => ({ type: "User", id })),
+          ];
+        } else return [{ type: "User", id: "LIST" }];
+      },
+    }),
+  }),
+});
+
+export const { useGetUsersQuery } = usersApiSlice;
+
+// returns the query result object
+export const selectUsersResult = usersApiSlice.endpoints.getUsers.select();
+
+// creates memoized selector
+const selectUsersData = createSelector(
+  selectUsersResult,
+  (usersResult) => usersResult.data // normalized state object with ids & entities.
+);
+
+// getSelectors creates these selectors and we rename them with aliases using destructuring
+
+export const {
+  selectAll: selectAllUsers,
+  selectById: selectUserById,
+  selectIds: selectUserIds,
+  // Pass in a selector that returns the users slice of state
+} = usersAdapter.getSelectors(
+  (state) => selectUsersData(state) ?? initialState
+);
