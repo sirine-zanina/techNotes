fc227f792422b93928410d348dadfabfc22bb059
reverie-kyrene <107148650+reverie-syr@users.noreply.github.com>
2023-10-22 19:31:10 +0200

    added UsersList

---
+++ client/src/features/users/User.js
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+
+import { useSelector } from "react-redux";
+import { selectUserById } from "./usersSlice";
+
+
+const User = () => {
+  return (
+    <div>User</div>
+  )
+}
+
+export default User
